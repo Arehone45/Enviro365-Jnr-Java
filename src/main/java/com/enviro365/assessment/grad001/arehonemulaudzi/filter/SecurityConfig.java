17b872f4d081c99cccd4e590f518033835ddcd76
Arehone45 <arehonemulaudzi08@gmail.com>
2024-02-21 20:01:38 +0200

    Added onceperrequest filter

---
+++ src/main/java/com/enviro365/assessment/grad001/arehonemulaudzi/filter/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.enviro365.assessment.grad001.arehonemulaudzi.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.requestMatchers("/**").authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        //.addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+
+    }
+}
